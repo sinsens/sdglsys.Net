6fdc69fad840173616a411a9e2e38783eebb9442
sinsens <xzapk@hotmail.com>
2018-07-31 23:23:28 +0800

    Web后台登录页使用静态页面

--- sdglsys.Web/Controllers/HomeController.cs
+++ sdglsys.Web/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace sdglsys.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public void Index()
         {
             /// #trial
             if (!XUtils.IsTrial)
@@ -16,7 +16,7 @@ namespace sdglsys.Web.Controllers
                 Response.Write("非常抱歉地提示您，您可能未经授权就使用了我的程序，或者该程序已到期，已经无法使用，现在是：" + DateTime.Now + "<br/>如有任何疑问，请联系QQ：1278386874");
                 Response.End();
             }
-            return View();
+            Server.TransferRequest("~/Login.html"); // 跳转到登录页面
         }
     }
 }
